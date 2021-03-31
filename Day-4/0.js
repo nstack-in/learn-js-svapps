c52422515ef420a1aea963190878e632e5fb3974
Nitish Kumar Singh <nitishk72@gmail.com>
2021-03-31 16:25:03 +0530

    day-4 array

---
+++ Day-4/0.js
@@ -0,0 +1,26 @@
+// let username = "ABCDEFGH";
+// console.log(username[5]);
+// let first = username.substr(5, 2);
+
+// console.log(first);
+
+// let operation = "DIVIDE";
+
+// if (operation == "ADD") {
+//    let ans = a / b;
+//    return ans;
+//    console.log("User want to do addition");
+// } else if (operation == "SUB") {
+//    let ans = a - b;
+//    return ans;
+//    console.log("User want to do subtraction");
+// } else if (operation == "PRODUCT") {
+//    console.log("User want to do multiplication");
+// } else if (operation == "DIVIDE") {
+//    console.log("User want to do division");
+// }
+
+// logical("ADD", 1, 2); // 3
+// logical("SUB", 5, 3); // 2
+// logical("MUL", 3, 3); // 9
+// logical("DIV", 9, 3); // 3
--- Day-4/01.js
+++ Day-4/01.js
@@ -1,1 +1,96 @@
 // Array and Object
+
+// let arr = [1, 3, "String", 3.4, false];
+// let len = arr.length;
+// console.log(len);
+
+// let users = ["Syed", "Sadguna", "Gopi", "Shirisha"];
+
+// for (let i = 0; i < users.length; i++) {
+//   let username = users[i];
+//   console.log(username);
+// }
+
+// let users = ["Syed", "Sadguna", "Gopi", "Shirisha"];
+
+// users.forEach(function (username) {
+//   console.log(username);
+// });
+
+// let numbers = [1, 4, 6, 8, 9, 11];
+
+// numbers.forEach(function (n) {
+//   let cube = n * n * n;
+//   console.log(cube);
+// });
+
+// let users = [];
+
+// users.push("Syed");
+// users.push("Sadguna");
+// users.pop();
+// users.push("Gopi");
+// users.pop();
+// users.push("Shirisha");
+
+// console.log(users);
+
+// let a = 10;
+// let b = a;
+
+// console.log(a); // 10
+// console.log(b); // 10
+
+// a = 20;
+
+// console.log(a); // 20
+// console.log(b); // 10
+
+// let a = [10, 20];
+// let b = a;
+
+// a.push(30);
+
+// console.log(a); // [10, 20, 30]
+// console.log(b); // [10, 20, 30]
+
+// let a = [10, 20];
+// let b = a;
+
+// console.log(a); // [10, 20]
+// console.log(b); // [10, 20]
+
+// a.pop();
+
+// console.log(a); // [10]
+// console.log(b); // [10]
+
+// let a = [10, 20];
+// let b = a;
+// a.push(99);
+// let c = Array.from(a);
+
+// a.push(30);
+
+// console.log(a); // [10,20,99,30]
+// console.log(b); // [10,20,99,30]
+// console.log(c); // [10,20,99]
+
+// let a = [10, 20];
+// let b = a;
+
+// b.push(99);
+
+// console.log(a); // 10,20, 99
+// console.log(b); // 10, 20, 99
+
+let a = [2, 5, 3, 7];
+let result = [];
+
+a.forEach(function (n) {
+  let square = n * n;
+  result.push(square);
+});
+
+console.log(a);
+console.log(result);
