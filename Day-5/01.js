61099f55fef5f6312caf07d5f3f48ffcd03a7df4
Nitish Kumar Singh <nitishk72@gmail.com>
2021-04-02 16:58:03 +0530

    day 5

--- Day-5/01.js
+++ Day-5/01.js
@@ -1,1 +1,65 @@
-// ES6 import
+// Objects
+
+// let user = {};
+// console.log(user);
+
+// key value pair
+// let user = {
+//   name: "Nitish",
+//   age: 10,
+//   location: "India",
+// };
+// let age = user["age"];
+// console.log(age);
+
+// let location = user.location;
+// console.log(location);
+
+// let user = {
+//   name: "Nitish",
+//   age: 10,
+//   location: "India",
+// };
+
+// user.location = "Delhi";
+// user["age"] = 20;
+// console.log(user);
+
+// let user = {
+//   name: "Nitish",
+//   age: 10,
+//   location: "India",
+// };
+
+// user["hobby"] = "Teaching";
+// user.hobby = "Teaching";
+
+// console.log(user);
+
+// let user = {};
+
+// user.name = "sayed";
+// user.age = 10;
+// console.log(user);
+
+// let user = {
+//   name: "Nitish",
+//   age: 10,
+//   location: "India",
+// };
+
+// let keys = Object.keys(user);
+// console.log(keys.length);
+
+// let user = {
+//   name: "Nitish",
+//   age: 10,
+//   location: "India",
+// };
+
+// let keys = Object.keys(user); // [ "name", "age", "location"]
+
+// keys.forEach(function (k) {
+//   let x = user[k];
+//   console.log(x);
+// });
